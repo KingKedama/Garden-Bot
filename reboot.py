ad578138b3fc830b1965e384e9596c100e81bcb1
Kedama <roaikgid@gmail.com>
2013-06-12 17:09:43 -0400

    fixed reboot

--- reboot.py
+++ reboot.py
@@ -1,11 +1,10 @@
 from command import *
-import os,sys
+import os,sys,subprocess
 class Reboot(Command):
 
     admin=True
     
     def run(self,sender,msg,target):
         self.cmdprocessor.outqueue.put((0,"QUIT rebooting"))
-        #subprocess.call(["python", "gardenbot.py", "-d", self.database])
-        os.system("python "+sys.argv[0]+" gardenbot.py -d "+self.database)
+        os.system("python "+sys.argv[0]+' -d '+ self.database)
         sys.exit(0)
